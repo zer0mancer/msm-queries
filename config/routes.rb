bb735cba6d180df4c25f38b4405256225e74fb32
Jim Vongsouvanh <95146291+zer0mancer@users.noreply.github.com>
2024-12-11 03:02:21 +0000

    added movies index

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
 
   get("/directors/:the_id", {:controller => "directors", :action => "show"})
 
+  get("/movies", {:controller => "movies", :action => "index"})
+
+  get("/movies/:the_id", {:controller => "movies", :action => "show"})
+
 end
